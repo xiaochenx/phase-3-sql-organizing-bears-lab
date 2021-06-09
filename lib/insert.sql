d5ab5d1f6b079ead7b4db46a46d8a43f1abfbfb4
Xiaochen Xu <xiaochex@gmail.com>
2021-06-08 21:56:17 -0400

    commit tree ce710f419234d14ea841d52f5e955bdfe12550eb

---
+++ lib/insert.sql
@@ -0,0 +1,8 @@
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ("AA", 3 , "F", "Brown", "Gentle", 1);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ("BB", 5 , "M", "Black", "Shy", 1);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ("CC", 1 , "F", "Brown", "Aggressive", 1);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ("DD", 9 , "M", "Brown", "Happpy", 1);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ("EE", 25 , "F", "Brown", "Mellow", 0);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ("FF", 17 , "M", "Brown", "Slow", 1);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ("GG", 12 , "F", "Brown", "Fast", 1);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES (null, 20 , "M", "Brown", "Aggressive", 1);
